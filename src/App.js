003404784a799b8b6d66c06419ecdca38162ff7e
Osher <105375761+Oosher@users.noreply.github.com>
2023-10-30 13:14:15 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
+
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Layout from './layout/Layout';
+import Router from './routes/Router';
+import UserProvider from './users/provider/UserProvider';
+import ProductsProvider from './products/providers/ProductsProvider';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename='/frontend'>
+      <UserProvider>
+        <ProductsProvider>
+          <Layout>
+            <Router/>
+          </Layout>
+        </ProductsProvider>
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
